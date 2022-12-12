ed5a2838689f0902d2d214fd2e7be9a43209bdc1
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2022-12-12 11:13:47 -0500

    [SOL] Define __ELF__ macro for the SBF target

--- clang/lib/Basic/Targets/BPF.cpp
+++ clang/lib/Basic/Targets/BPF.cpp
@@ -78,6 +78,10 @@ void BPFTargetInfo::getTargetDefines(const LangOptions &Opts,
     Builder.defineMacro("__BPF_FEATURE_ST");
     Builder.defineMacro("__BPF_FEATURE_LOAD_ACQ_STORE_REL");
   }
+
+  if (HasSolanaFeature) {
+    Builder.defineMacro("__ELF__");
+  }
 }
 
 static constexpr llvm::StringLiteral ValidCPUNames[] = {"generic", "v1", "v2",
