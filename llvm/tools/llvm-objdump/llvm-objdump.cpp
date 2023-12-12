2530fc069c4ac81e98bf98152e6df4677233d8ce
Jason Eckhardt <jason.eckhardt@solana.com>
2023-12-12 01:22:19 -0600

    [SOL] Remove now obsolete old syntax bits. (#72)
    Patch https://github.com/solana-labs/llvm-project/pull/54 originally
    reworked the SBF textual assembly syntax to match the rbpf-style syntax.
    
    In order to allow some soak-time, the above patch temporarily supported
    both the old and new syntax (selectable on the command line or via bespoke
    assembler directives). A little over a year has passed since then,
    and all is well with the new syntax, testing, and so forth.
    
    The current patch now removes all existing remnants of the old syntax--
    including some of the target-independent changes made in the original
    patch to support both (e.g., a minor TableGen change for variants, some
    additional command line selector flags, etc).
    
    All related unit tests have been updated accordingly.

--- llvm/tools/llvm-objdump/llvm-objdump.cpp
+++ llvm/tools/llvm-objdump/llvm-objdump.cpp
@@ -348,8 +348,6 @@ bool objdump::UnwindInfo;
 static bool Wide;
 std::string objdump::Prefix;
 uint32_t objdump::PrefixStrip;
-static uint64_t OutputAsmVariant;
-static bool HasOutputAsmVariantFlag;
 
 DebugVarsFormat objdump::DbgVariables = DVDisabled;
 
@@ -1089,8 +1087,6 @@ DisassemblerTarget::DisassemblerTarget(const Target *TheTarget, ObjectFile &Obj,
   InstrAnalysis.reset(TheTarget->createMCInstrAnalysis(InstrInfo.get()));
 
   int AsmPrinterVariant = AsmInfo->getAssemblerDialect();
-  if (HasOutputAsmVariantFlag)
-    AsmPrinterVariant = OutputAsmVariant;
   InstPrinter.reset(TheTarget->createMCInstPrinter(Triple(TripleName),
                                                    AsmPrinterVariant, *AsmInfo,
                                                    *InstrInfo, *RegisterInfo));
@@ -3576,8 +3572,6 @@ static void parseObjdumpOptions(const llvm::opt::InputArgList &InputArgs) {
   }
 
   parseIntArg(InputArgs, OBJDUMP_debug_vars_indent_EQ, DbgIndent);
-  parseIntArg(InputArgs, OBJDUMP_output_asm_variant_EQ, OutputAsmVariant);
-  HasOutputAsmVariantFlag = InputArgs.hasArg(OBJDUMP_output_asm_variant_EQ);
 
   parseMachOOptions(InputArgs);
 
