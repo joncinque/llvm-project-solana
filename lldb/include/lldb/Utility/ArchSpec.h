176f4c210ee2e2961265b2335f4c296ea6840e1d
Lucas Ste <38472950+LucasSte@users.noreply.github.com>
2025-04-11 14:30:31 -0300

    [SOL] LLDB for LLVM 19 (#145)
    * [SOL] lldb: pretty print instructions
    
    * [SOL] lldb: skip ranges with DW_AT_low_pc equal to zero
    
    * [SOL] lldb: Add solana-lldb wrapper
    
    This adds a wrapper around the LLDB executable that resides in the same directory. Once we start to distribute LLDB in sbf-tools, the wrapper + scripts can be put into the same directory as the LLDB executable. Invoking ./solana-lldb will pre-load helper functions to print rust as well as solana types (e.g. pubkeys in base58 or auto deref of accounts) and then start the LLDB executable.
    
    * [SOL] Revamp LLDB for SBF
    
    ---------
    
    Co-authored-by: wj <wzggUaPFt5J7f@gmx.de>

--- lldb/include/lldb/Utility/ArchSpec.h
+++ lldb/include/lldb/Utility/ArchSpec.h
@@ -107,8 +107,10 @@ public:
   };
 
   enum SBFSubType {
-    eSBFSubType_sbf,
+    eSBFSubType_sbfv0,
+    eSBFSubType_sbfv1,
     eSBFSubType_sbfv2,
+    eSBFSubType_sbfv3,
   };
 
   enum RISCVSubType {
@@ -234,8 +236,10 @@ public:
     eCore_wasm32,
 
     eCore_bpf,
-    eCore_sbf,
+    eCore_sbfv0,
+    eCore_sbfv1,
     eCore_sbfv2,
+    eCore_sbfv3,
 
     kNumCores,
 
